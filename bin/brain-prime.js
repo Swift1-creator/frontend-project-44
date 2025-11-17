4224a81dfd038f4d4d0fd447f4fb6033c0584638
Никифор Суханов <kik.ss@yandex.ru>
2025-11-17 23:22:36 +0300

    .

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,48 +1,37 @@
 #!/usr/bin/env node
 
-const readline = require('node:readline');
+import readlineSync from 'readline-sync';
 
 function isPrime(num) {
   if (num < 2) return false;
-  for (let i = 2; i <= Math.sqrt(num); i++) {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) return false;
   }
   return true;
 }
 
-function askQuestion(query) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  return new Promise((resolve) => rl.question(query, (ans) => {
-    rl.close();
-    resolve(ans);
-  }));
-}
-
-async function main() {
-  console.log('Welcome to the Brain Games!');
-  const name = await askQuestion('May I have your name? ');
+function brainPrime() {
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  const totalQuestions = 3;
 
-  let continueGame = true;
+  for (let i = 0; i < totalQuestions; i += 1) {
+    const number = Math.floor(Math.random() * 100) + 1;
+    const correctAnswer = isPrime(number) ? 'yes' : 'no';
 
-  while (continueGame) {
-    const number = Math.floor(Math.random() * 100) + 1; // число от 1 до 100
-    const answer = isPrime(number) ? 'yes' : 'no';
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
 
-    const userAnswer = await askQuestion(`Question: ${number}\nYour answer: `);
-
-    if (userAnswer.trim().toLowerCase() === answer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+    if (answer.toLowerCase() !== correctAnswer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
-      continueGame = false;
+      return;
+    } else {
+      console.log('Correct!');
     }
   }
+
+  console.log(`Congratulations, ${name}!`);
 }
 
-main();
+brainPrime();
