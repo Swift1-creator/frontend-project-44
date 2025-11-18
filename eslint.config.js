a954b398242ce95da60f9313cfa5dd4c28cdb123
Никифор Суханов <kik.ss@yandex.ru>
2025-11-18 22:40:30 +0300

    .

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+
+export default defineConfig([
+  {
+    files: ["**/*.js"],
+    plugins: {
+      js,
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        console: "readonly",
+        process: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+]);
--- eslint.config.mjs
+++
@@ -1,7 +0,0 @@
-// eslint.config.mjs
-export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    rules: {},
-  }
-]
