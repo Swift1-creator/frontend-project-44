2cd398783114cd24504ebe8b8149629ef7bf6433
Никифор Суханов <kik.ss@yandex.ru>
2025-11-18 22:13:53 +0300

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,24 +2,6 @@
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    languageOptions: {
-      parserOptions: {
-        ecmaVersion: 2021,
-        sourceType: "module",
-      },
-    },
-    rules: {
-      semi: "off",
-      indent: "off",
-      "brace-style": "off",
-      "no-multiple-empty-lines": "off",
-      "arrow-parens": "off",
-      "quotes": "off",
-      "no-trailing-spaces": "off",
-      "no-dupe-keys": "off",
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-uses-react": "off",
-      "react/jsx-uses-vars": "off",
-    },
+    rules: {},
   }
 ]
