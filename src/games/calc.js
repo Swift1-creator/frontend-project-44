b5a0747bf025705fd3fb583282746ea5309e8b3c
Никифор Суханов <kik.ss@yandex.ru>
2025-11-18 23:16:53 +0300

    .

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,24 +1,27 @@
-import { randomInt }
-  from '../utils.js'
+import { randomInt } from '../utils.js'
+
 const operators = ['+', '-', '*']
+
 export const getQuestionAndAnswer = () => {
   const num1 = randomInt(1, 100)
   const num2 = randomInt(1, 100)
   const operator = operators[randomInt(0, operators.length - 1)]
   let answer
+
   switch (operator) {
     case '+':
-      answer = num1 + num2;
-    break
+      answer = num1 + num2
+      break
     case '-':
       answer = num1 - num2
       break
-    case '*'
+    case '*':
       answer = num1 * num2
       break
     default:
       answer = 0
   }
+
   const question = `${num1} ${operator} ${num2}`
   return { question, correctAnswer: answer }
 }
