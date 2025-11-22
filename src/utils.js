3e575ce10fa0cbedef9116ca8172235234dacfb3
Никифор Суханов <kik.ss@yandex.ru>
2025-11-22 21:21:44 +0300

    .

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,1 @@
-export const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+export const randomInt = (min = 0, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min
